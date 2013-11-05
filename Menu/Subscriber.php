56dfdfcf1dc30a7259698de9907bf9f7dd7efe84
Krystian Kuczek <krystian@kuczek.eu>
2013-11-05 19:08:11 +0100

    Messages and routes.

--- Menu/Subscriber.php
+++ Menu/Subscriber.php
@@ -7,29 +7,17 @@ use Hexmedia\AdministratorBundle\Menu\Event;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Hexmedia\AdministratorBundle\Menu\Subscriber as SubscriberAbstract;
 
-class Subscriber extends SubscriberAbstract implements EventSubscriberInterface
-{
+class Subscriber extends SubscriberAbstract implements EventSubscriberInterface {
 
-	public function addPositions(Event $event)
-	{
+	public function addPositions(Event $event) {
 
-		$menu = $event->getMenu()->addChild($this->translator->trans("Media Library"), ['route' => 'HexMediaContentMedia', 'id' => 'HexMediaContentMedia']);
-//		$menu->addChild($this->translator->trans("Media"), ['route' => 'HexMediaContentMedia', 'under' => 'HexMediaContentMedia']);
-		$menu->addChild($this->translator->trans("Add"), ['route' => 'HexMediaContentMediaAdd', 'under' => 'HexMediaContentMedia']);
+		$menu = $event->getMenu()->addChild($this->translator->trans("Media Library"), ['route' => 'HexMediaContentMedia', 'id' => 'HexMediaContentMedia'])->setAttribute('icon', 'icon-picture');
+		$menu = $event->getMenu()->addChild($this->translator->trans("Page"), ['route' => 'HexMediaContentPage', 'id' => "HexMediaContentPage"])->setAttribute('icon', 'icon-sitemap');
 
-		$menu = $event->getMenu()->addChild($this->translator->trans("Area"), ['route' => 'HexMediaContentArea', 'id' => 'HexMediaContentArea']);
-		$menu->addChild($this->translator->trans("Add"), ['route' => 'HexMediaContentAreaAdd']);
-
-        $menu = $event->getMenu()->addChild($this->translator->trans("Page"), ['route' => 'HexMediaContentPage', 'id' => "HexMediaContentPage"]);
-        $menu->addChild($this->translator->trans("Add"), ['route' => 'HexMediaContentPageAdd']);
-
-        return $event->getMenu();
-
-
+		return $event->getMenu();
 	}
 
-	public static function getSubscribedEvents()
-	{
+	public static function getSubscribedEvents() {
 		return array(
 			Builder::MENU_BUILD_EVENT => array('addPositions', 4)
 		);
